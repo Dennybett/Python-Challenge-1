2964ee6f232e78af3f5201f3bef00fb2657555a6
Dennybett <dennobett@gmail.com>
2024-10-27 15:14:20 -0400

    Modified python script

--- menu.py
+++ menu.py
@@ -52,14 +52,20 @@ menu = {
 
 # 1. Set up order list. Order list will store a list of dictionaries for
 # menu item name, item price, and quantity ordered
-
+order_list = []
 
 # Launch the store and present a greeting to the customer
 print("Welcome to the variety food truck.")
 
 # Customers may want to order multiple items, so let's create a continuous
 # loop
-place_order = True
+user_input = input("Would you like to place an order? Press (y) to\
+ continue: ")
+if user_input.lower() == 'y':
+    place_order = True
+else:
+    place_order = False    
+
 while place_order:
     # Ask the customer from which menu category they want to order
     print("From which menu would you like to order? ")
@@ -117,47 +123,75 @@ while place_order:
                         "Price": value
                     }
                     i += 1
-            # 2. Ask customer to input menu item number
 
+
+            # 2. Ask customer to input menu item number
+            menu_item_number= input ("Enter the number corresponding to your selection ")
 
             # 3. Check if the customer typed a number
-
+            if menu_item_number.isdigit():
                 # Convert the menu selection to an integer
-
-
+                menu_item_number = int(menu_item_number)
                 # 4. Check if the menu selection is in the menu items
-
+                if (menu_item_number) in menu_items.keys():
                     # Store the item name as a variable
-
+                    menu_item_details= menu_items[menu_item_number]
+                    menu_item_name = menu_item_details["Item name"]
+                    price= menu_item_details["Price"]
 
                     # Ask the customer for the quantity of the menu item
-
+                    quantity_input= (input(f"How many {menu_item_name} did you want? "))
 
                     # Check if the quantity is a number, default to 1 if not
+                    try: 
+                        quantity= int(quantity_input)
+                    except ValueError:
+                        quantity = 1
 
+                    # Debug for what is being added to the list
+                    print(f"Adding {quantity} x {menu_item_name} at ${price:.2f} to order list.")
 
                     # Add the item name, price, and quantity to the order list
-
-
+                    order_list.append({
+                     "Item name": menu_item_name,
+                     "Price": price,
+                     "Quantity": quantity
+                     })
+                else:
                     # Tell the customer that their input isn't valid
-
+                    print("The menu item number you selected is not valid.")
+                    continue
 
                 # Tell the customer they didn't select a menu option
+            else:
+                print("The selection you put is not valid ")
+                continue
 
         else:
             # Tell the customer they didn't select a menu option
             print(f"{menu_category} was not a menu option.")
+            continue
     else:
         # Tell the customer they didn't select a number
         print("You didn't select a number.")
+        continue
 
     while True:
         # Ask the customer if they would like to order anything else
-        keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ")
-
+        keep_ordering = input("Would you like to order anything else?: y/n ")
+        
         # 5. Check the customer's input
-
+        
                 # Keep ordering
+        if keep_ordering.lower() == 'n':
+            print("Thank you for your order")
+            place_order = False
+            break
+        elif keep_ordering.lower() == 'y':
+            print("Let's continue with your order")
+            break
+        else:
+            print("Invalid input. Please enter 'y' or 'n'")
 
                 # Exit the keep ordering question loop
 
@@ -171,18 +205,37 @@ while place_order:
 
                 # Tell the customer to try again
 
-
 # Print out the customer's order
-print("This is what we are preparing for you.\n")
-
+if len(order_list) > 0:
+    print("This is what we are preparing for you:\n")
+    for item in order_list:
+        item_name = item["Item name"]
+        price = item["Price"]
+        quantity = item["Quantity"]
+        print(f"{quantity} x {item_name} at ${price:.2f} each")
 # Uncomment the following line to check the structure of the order
 #print(order)
 
-print("Item name                 | Price  | Quantity")
-print("--------------------------|--------|----------")
+    print("Item name                 | Price  | Quantity")
+    print("--------------------------|--------|----------")
+    total = 0
+    for item in order_list:
+        item_name = item["Item name"]
+        price = item["Price"]
+        quantity = item["Quantity"]
+        num_item_spaces = 25 - len(item_name)
+        item_spaces = " " * num_item_spaces
+        num_price_space = 6 - len(str(price)) 
+        price_space = " " * num_price_space
+        total = total + price * quantity
+        print(f"{item_name}{item_spaces} | ${price}{price_space}| {quantity}")
+        
+    # 6. Loop through the items in the customer's order
+    print("-" * 46)
+    print(" " * 2)
+    print(f"Your total is: ${total:.2f}")
 
-# 6. Loop through the items in the customer's order
-
+print(" " * 2)
     # 7. Store the dictionary items as variables
 
 
@@ -195,6 +248,9 @@ print("--------------------------|--------|----------")
     # 10. Print the item name, price, and quantity
 
 
-# 11. Calculate the cost of the order using list comprehension
-# Multiply the price by quantity for each item in the order list, then sum()
-# and print the prices.
+    # 11. Calculate the cost of the order using list comprehension
+    # Multiply the price by quantity for each item in the order list, then sum()
+    # and print the prices.
+
+print("Thank you for stopping at variety food truck! See you next time!")
+print(" " * 2)
